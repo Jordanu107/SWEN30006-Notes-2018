84cfb3aa72a8d28eb9ac5d86e1e9265f4c6344c4
Jordanu107 <jordanu@student.unimelb.edu.au>
2018-08-11 23:26:10 +1000

    Added seed file and custom file to write project code

---
+++ Assignments/SWEN30006_2018S2_PartA/swen30006/strategies/MyMailPool.java
@@ -0,0 +1,32 @@
+package strategies;
+
+import automail.MailItem;
+import automail.Robot;
+
+public class MyMailPool implements IMailPool {
+
+	@Override
+	public void addToPool(MailItem mailItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerWaiting(Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deregisterWaiting(Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
